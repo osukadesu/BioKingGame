a616119d7344ebb2c9c5d58fc9fe2a87ae6e06ee
osukadesu <64621867+osukadesu@users.noreply.github.com>
2022-11-16 17:13:28 -0500

    Full

--- Assets/MyAssets/Scripts/Misiones/Mision2.cs
+++ Assets/MyAssets/Scripts/Misiones/Mision2.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mision2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(3);
+        }
     }
 }
---
+++ Assets/MyAssets/Scripts/back.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class back : MonoBehaviour
+{private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void MyBack()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
